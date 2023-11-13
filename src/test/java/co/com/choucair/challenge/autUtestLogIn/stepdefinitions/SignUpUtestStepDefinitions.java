4b96afa460e3573ae776b9a15e68002dbf8595c6
CaroAlexander <yatobarc@correo.udistrital.edu.co>
2023-11-13 16:03:56 -0500

    se implementa DataDriven en el proyecto

--- src/test/java/co/com/choucair/challenge/autUtestLogIn/stepdefinitions/SignUpUtestStepDefinitions.java
+++ src/test/java/co/com/choucair/challenge/autUtestLogIn/stepdefinitions/SignUpUtestStepDefinitions.java
@@ -1,5 +1,6 @@
 package co.com.choucair.challenge.autUtestLogIn.stepdefinitions;
 
+import co.com.choucair.challenge.autUtestLogIn.model.AcademyChoucairData;
 import co.com.choucair.challenge.autUtestLogIn.questions.LoginQuestions;
 import co.com.choucair.challenge.autUtestLogIn.tasks.*;
 import cucumber.api.java.Before;
@@ -10,29 +11,41 @@ import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 
+import java.util.List;
+
 public class SignUpUtestStepDefinitions {
 
     @Before
     public void setStage (){ OnStage.setTheStage(new OnlineCast());}
 
-    @Given("^that Alex is on the Utest homepage$")
-    public void thatAlexWantsToLogInToTheJoinTodaySectionOnTheUtestComPage() {
-        OnStage.theActorCalled("Alex").wasAbleTo(OpenUp.thePage());
+    @Given("^that (.*) the actor is on the Utest homepage$")
+    public void thatAlexWantsToLogInToTheJoinTodaySectionOnTheUtestComPage(String actor) throws Exception {
+        OnStage.theActorCalled(actor).wasAbleTo(OpenUp.thePage());
     }
 
     @When("^he successfully completes the whole sign up process$")
-    public void heEnterIntoTheJoinTodaySectionOnTheUtestComPage() throws InterruptedException {
+    public void heEnterIntoTheJoinTodaySectionOnTheUtestComPage(List<AcademyChoucairData> academyChoucairData) throws InterruptedException {
         OnStage.theActorInTheSpotlight().
                 attemptsTo(
-                        (TellingAboutYourself.OnTheStep1()),
-                        (AddingYourAddress.OnTheStep2()),
+                        (TellingAboutYourself.OnTheStep1(
+                                academyChoucairData.get(0).getStrFirstName(),
+                                academyChoucairData.get(0).getStrLastName(),
+                                academyChoucairData.get(0).getStrEmail()
+                        )),
+                        (AddingYourAddress.OnTheStep2(
+                                academyChoucairData.get(0).getStrCity(),
+                                academyChoucairData.get(0).getStrZipCode()
+
+                        )),
                         (TellingAboutYourDevices.OnTheStep3()),
-                        (DoingTheLast.OnTheLastStep())
+                        (DoingTheLast.OnTheLastStep(
+                                academyChoucairData.get(0).getStrPassword()
+                        ))
                 );
     }
 
     @Then("^the welcome message is displayed out$")
-    public void heFillsOutTheForm() {
+    public void heFillsOutTheForm(List<AcademyChoucairData> academyChoucairData) throws Exception {
 
         String SUB_TITLE = "Welcome to the world's largest community of freelance software testers!";
 
--- src/test/resources/features/SignUpUtest.feature
+++ src/test/resources/features/SignUpUtest.feature
@@ -4,6 +4,10 @@
     As a Tester, I want to sign up  on the Utest.com page to create a account
     @scenario1
     Scenario: Sing up for a new user by clicking Join Today button
-      Given that Alex is on the Utest homepage
+      Given that Alex the actor is on the Utest homepage
       When he successfully completes the whole sign up process
+        | strFirstName   | strLastName | strEmail              | strCity | strZipCode | strPassword            |
+        | Yair Alexander | Tobar Caro  | yatobarcaro@gmail.com | Bogota  | 111041     | HiChoucairISucceed2023 |
       Then the welcome message is displayed out
+        | strWelcomeMessage                                                       |
+        | Welcome to the world's largest community of freelance software testers! |
